8aa67c3e287a4bfd15a78e5150dbf8affb94795e
guillaume barillot <gbarillot@gmail.com>
2017-12-02 18:07:55 +0100

    Cleanup

--- config.rb
+++ config.rb
@@ -2,8 +2,6 @@
 # Blog settings
 ###
 
-# Time.zone = "UTC"
-
 activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
   # blog.prefix = "blog"
@@ -68,53 +66,25 @@ ready do
 end
 
 ###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", layout: false
-#
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
-
-###
 # Helpers
 ###
 
 # Automatic image dimensions on image_tag helper
 # activate :automatic_image_sizes
-
 # Reload the browser automatically whenever files change
 activate :livereload
-
 # Pretty URLs - http://middlemanapp.com/basics/pretty-urls/
 activate :directory_indexes
+activate :syntax, line_numbers: true
 
 # Middleman-Syntax - https://github.com/middleman/middleman-syntax
 set :haml, { ugly: true }
 set :markdown_engine, :redcarpet
 set :markdown, fenced_code_blocks: true, smartypants: true
-activate :syntax, line_numbers: true
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
+set :partials_dir, 'partials'
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -122,14 +92,6 @@ activate :syntax, line_numbers: true
 #     "Helping"
 #   end
 # end
-
-set :css_dir, 'stylesheets'
-
-set :js_dir, 'javascripts'
-
-set :images_dir, 'images'
-
-set :partials_dir, 'partials'
 
 # Build-specific configuration
 configure :build do
